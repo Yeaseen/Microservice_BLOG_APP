13273f5a0c38f9a05e1c9b36ceb9ea27eecd9874
Yeaseen <yeaseen.arafat96@gmail.com>
2021-10-23 21:44:19 +0600

    working perfect

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -8,8 +8,8 @@ const PostList = () => {
   const [posts, setPosts] = useState({})
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts')
-
+    const res = await axios.get('http://localhost:4002/posts')
+    console.log(res.data)
     setPosts(res.data)
   }
 
@@ -29,7 +29,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.postId} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.postId} />
         </div>
       </div>
